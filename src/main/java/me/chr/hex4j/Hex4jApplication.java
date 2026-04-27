3826b50c33cb09fb41c67002e1a6cc3d216e46f7
Visualer <1252699590@qq.com>
2026-04-27 14:43:59 +0800

    init

---
+++ src/main/java/me/chr/hex4j/Hex4jApplication.java
@@ -0,0 +1,13 @@
+package me.chr.hex4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hex4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hex4jApplication.class, args);
+    }
+
+}
